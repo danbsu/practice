037a5d6e81482299e9bae4229e0d4fce16738dbc
danbsu <daniel.morency@bsu.edu>
2025-02-03 13:06:14 -0500

    Boilerplate

---
+++ script.js
@@ -0,0 +1,5 @@
+// This is a simple JavaScript file for future enhancements
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document is ready!');
+    // Add any interactive features here
+}); 
